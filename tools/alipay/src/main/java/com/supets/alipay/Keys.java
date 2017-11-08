ca8ae90d21abb9fa7f4f8316719f9a3a2a4f9842
李洪江 <lihongjiang@supets.com>
2017-11-08 14:55:00 +0800

    init

--- tools/alipay/src/main/java/com/supets/alipay/Keys.java
+++ tools/alipay/src/main/java/com/supets/alipay/Keys.java
@@ -18,9 +18,9 @@ package com.supets.alipay;
 public final class Keys {
 
 	//合作身份者id，以2088开头的16位纯数字
-	public static final String DEFAULT_PARTNER = "2088621664100828";   // 支付宝国内PID
+	public static final String DEFAULT_PARTNER = "2088821129922541";   // 支付宝国内PID
 	//收款支付宝账号
-	public static final String DEFAULT_SELLER = "wcnxzhifubao@supets.com";
+	public static final String DEFAULT_SELLER = "yczhifubao@supets.com";
 	//商户私钥，自助生成
 	public static  String PRIVATE = "";
 	public static final String PUBLIC = "";
